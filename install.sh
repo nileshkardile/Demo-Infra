f99c4d4ca65ed8d2758fb04c6963b4233058d916
sandeep2suri <60914874+sandeep2suri@users.noreply.github.com>
2020-02-17 17:03:55 +0530

    Update install.sh
    updated file to move third party framework changes to separate repo

--- install.sh
+++ install.sh
@@ -10,12 +10,4 @@ sudo /usr/bin/terraform plan;
 sudo /usr/bin/terraform apply -auto-approve;
 
 sudo /usr/bin/aws eks --region us-east-2 update-kubeconfig --name eks-demo
-sudo /home/ec2-user/istio-install/istio-1.4.3/bin/istioctl manifest apply --set profile=demo
 
-sudo /usr/bin/kubectl apply -f ./metrics-server-0.3.6/deploy/1.8+/
-
-cd ../third_party_framework
-sudo /usr/bin/kubectl apply -f grafana.yaml
-sudo /usr/bin/kubectl apply -f kaili.yaml
-sudo /usr/bin/kubectl apply -f prometheus.yaml
-sudo /usr/bin/kubectl apply -f tracing.yaml
