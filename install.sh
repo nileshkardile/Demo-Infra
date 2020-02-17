72f008d0003aeae9aa6c88dd80e85b20043be66f
sandeep2suri <60914874+sandeep2suri@users.noreply.github.com>
2020-02-17 17:17:00 +0530

    Update install.sh
    added some comments for readibility

--- install.sh
+++ install.sh
@@ -5,9 +5,12 @@ set -xv
 
 home_dir=/var/lib/jenkins/workspace/Demo-Infra
 cd $home_dir/src
+
+# create infra using terraform
 sudo /usr/bin/terraform init;
 sudo /usr/bin/terraform plan;
 sudo /usr/bin/terraform apply -auto-approve;
 
+# set EKS config
 sudo /usr/bin/aws eks --region us-east-2 update-kubeconfig --name eks-demo
 
